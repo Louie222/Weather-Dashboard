0fc1e078eae80e3549891ffddf69927a41ade7f3
Louie Obaldo <louie.obaldo@education.wa.edu.au>
2021-04-15 08:58:59 +0800

    Updating assignment with html layout and javacript

--- script.js
+++ script.js
@@ -9,4 +9,27 @@
 //THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 //WHEN I click on a city in the search history
 //THEN I am again presented with current and future conditions for that city
+var searchInput = document.querySelector("#search-input")
+var searchButton = document.querySelector("#search-button")
 
+searchButton.addEventListener("click", getCurrentWeather)
+
+function getCurrentWeather (){ 
+    var cityToSearch = searchInput.value
+    
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityToSearch}&appid=3547747b883ab13f08e1002874f2b8fc&units=metric`)
+  .then(response => response.json())
+  .then(data => {
+      console.log(data)
+
+      var currentWeather = data 
+        var temperature = document.querySelector("#temperature")
+        temperature.textContent = data.main.temp
+
+    });
+
+}
+
+
+
+//templateString
